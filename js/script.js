63c03e40330c9d4f801242d010a994e767f3519a
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-17 17:13:16 +0200

    remove change log to js, scaffolding html

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 //PALINDROMI
-    //creiamo caselle input su HTML, le dichiariamo su js
-    
-
-
+palindromi();
+function palindromi(){
 const btn = document.querySelector('button');
 const btnReset = document.getElementById('reset');
 const boxAlert = document.getElementById('answer');
 const data = document.getElementById('data');
-const btnPrint = document.getElementById('print');
 
 let arr = '';
 let arrInverse ='';
@@ -19,35 +16,46 @@ btn.addEventListener('click', function(){
     //ciclo for per scomporre le lettere della parola
     //le pushiamo dentro ARR
     for(let i = 0; i < dataValue.length ; i++){
-        console.log(dataValue[i]);
         arr += dataValue[i];
     }
 
     //ciclo for per scomporre le lettere della parola ma in ordine inverso, 
     //le pushiamo dentro ArrINVERSE
     for(let x = dataValue.length -1; x >= 0 ; x--){
-        console.log(dataValue[x]);
         arrInverse += dataValue[x];
     } 
 
     if(arr === arrInverse){
-        boxAlert.innerHTML = `la parola è palindroma`
+        boxAlert.innerHTML += `la parola è palindroma`
         boxAlert.classList.remove('d-none');
     } else{
-        boxAlert.innerHTML = `la parola non è palindroma`
+        boxAlert.innerHTML += `la parola non è palindroma`
         boxAlert.classList.remove('d-none');
     }
-
-    //CONSOLE.LOG 
-    //console.log(dataValue);  
-    //console.log(data);
-    console.log('array 1 ', arr);
-    console.log('arrayInverso ',arrInverse);
 });
-
 
 btnReset.addEventListener('click', function(){
     boxAlert.classList.add('d-none');
     boxAlert.innerHTML = '';
     data.value = '';
 });
+};
+
+//NUMERI
+numeri();
+function numeri(){
+const btn2 = document.getElementById('btn2')
+const btnReset2 = document.getElementById('reset2');
+const boxAlert2 = document.getElementById('answer2');
+const data2 = document.getElementById('data2');
+
+btn2.addEventListener('click', function(){
+    let dataValue2 = data2.value;
+    data2.value = '';
+    console.log(dataValue2);
+})
+
+
+
+
+};
